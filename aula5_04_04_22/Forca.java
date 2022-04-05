ad73e0e84ec631d981999d6345c4bc88a4a7046c
mateus <welter.mateus@gmail.com>
2022-04-04 23:40:50 -0300

    Trabalho -  Jogo da Forca - Inicio

---
+++ aula5_04_04_22/Forca.java
@@ -0,0 +1,10 @@
+public class Forca {
+    private String palavra;
+    private String dica;
+    private int tamPalavra;
+    private int limiteErros;
+    private int erros;
+    private char chute;
+    private char palavraAtual[];
+    private boolean statusTermino;
+}
